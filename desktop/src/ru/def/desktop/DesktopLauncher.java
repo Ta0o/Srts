425f28113b9ed8bd48864a63bdd19f5da329db54
Ta0o <dimas_s_s@list.ru>
2018-10-17 22:54:07 +0300

    1

---
+++ desktop/src/ru/def/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ru.def.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ru.def.mg;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new mg(), config);
+	}
+}
